4a668dd390049e8bfba9b4785bf88887fc7384c2
Sukjune Lee <robin00q@naver.com>
2020-01-08 15:52:42 +0900

    initial commit

---
+++ src/main/java/net/fion/FionApplication.java
@@ -0,0 +1,13 @@
+package net.fion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FionApplication.class, args);
+	}
+
+}
